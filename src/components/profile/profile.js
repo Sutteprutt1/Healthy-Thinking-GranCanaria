de0407156261042d84e7a7189c9f21b7c6dd462c
Jordan Jared Tejera Yánez <Darkjordanrayo@gmail.com>
2023-03-14 09:23:48 +0000

    Profile fixed

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -1,13 +1,34 @@
+import { useEffect, useState } from "react";
 import { Card } from "../activity-card/card/card";
 import { UserImg, UserName, UserWrapper } from "./styles";
+import ActivityService from "../../services/activity.service.js"
 
 export function UserProfile() {
+
+  const [activity, setActivity] = useState([]);
+
+  useEffect(() => {
+    retrieveEvent();
+  }, []);
+
+  const retrieveEvent = () => {
+    ActivityService.getAll()
+      .then(response => {
+        setActivity(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
 
   return (
     <UserWrapper>
       <UserImg src="https://i.imgur.com/1Q9ZQ2r.jpg" />
       <UserName>Your name</UserName>
-      <Card />
+      {activity &&
+        activity.map((event, index) => (
+          <Card key={index} activity={event} />
+        ))};
     </UserWrapper>
   );
 }
