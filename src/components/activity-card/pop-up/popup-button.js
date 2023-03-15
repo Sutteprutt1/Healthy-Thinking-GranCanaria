82ccf70da9b4ce7449ec03910a2cb5ddf324f3f7
Jordan Jared Tejera Yánez <Darkjordanrayo@gmail.com>
2023-03-15 00:23:35 +0000

    User will be able to see his activities in his agenda and the filter are prepared to show

--- src/components/activity-card/pop-up/popup-button.js
+++ src/components/activity-card/pop-up/popup-button.js
@@ -1,5 +1,26 @@
 import { Button } from "./styles.js";
+import SuscriptionService from "../../../services/suscription.service"
 
 export default function PopupButton(props) {
-  return <Button>Add to agenda</Button>;
+
+  // Receive the info of activity from the page
+  const activity = props.activity;
+
+  var data = {
+    start_time: '',
+    end_time: '',
+    userId: localStorage.getItem("userId"),
+    activityId: activity.id
+  }
+
+  function addToAgenda() {
+    console.log(data);
+    SuscriptionService.create(data).then(data => {
+      console.log('OK');
+    }).catch(err => {
+      console.log(err);
+    })
+  }
+
+  return <Button onClick={addToAgenda} >Add to agenda</Button>;
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import { Card } from "../components/activity-card/card/card";
 import ActivityService from "../services/activity.service.js";
+import FilterService from "../services/filter.service.js";
 import { BackgroundGradient } from "../components/globalStyles";
 import Navbar from "../components/navbar/navbar";
 import { PaidFilter } from "../components/paid-filter/paid-filter";
 
 export function Home() {
   const [activity, setActivity] = useState([]);
+  const [filter, setFilter] = useState([]);
 
   useEffect(() => {
     retrieveEvent();
@@ -20,6 +22,13 @@ export function Home() {
       .catch((e) => {
         console.log(e);
       });
+    FilterService.getAll()
+      .then((response) => {
+        setFilter(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
   };
 
   return (
@@ -28,8 +37,11 @@ export function Home() {
       <BackgroundGradient />
       <h1>Home</h1>
       <PaidFilter />
+      {filter &&
+        filter.map((event, index) => <></>)
+      }
       {activity &&
-        activity.map((event, index) => <Card key={index} activity={event} type="add"/>)}
+        activity.map((event, index) => <Card key={index} activity={event} type="add" />)}
     </>
   );
 }
