3b294467d905bd2fd323189e6a6e3d829595eec4
Lasse <munkholm222@hotmail.com>
2023-03-15 16:51:44 +0100

    something happend while merging

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
-<<<<<<< HEAD
 import "./App.css"
-import ReactDOM from "react-dom/client"
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import {Home} from "./pages/home"
 import {Profile} from "./pages/profile"
 import {Layout} from "./pages/layout"
 import {NoPage} from "./pages/no-page"
 import {Login} from "./pages/login"
-=======
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home } from "./pages/home";
-import { Profile } from "./pages/profile";
-import { Layout } from "./pages/layout";
-import { NoPage } from "./pages/no-page";
-import { Login } from "./pages/login";
->>>>>>> cf6269dae574cbc37e57e00c3202c7ccf2aae9e5
 
 export default function App() {
   return (
@@ -31,8 +20,3 @@ export default function App() {
     </BrowserRouter>
   )
 }
-<<<<<<< HEAD
-
-const root = ReactDOM.createRoot(document.getElementById("root"))
-=======
->>>>>>> cf6269dae574cbc37e57e00c3202c7ccf2aae9e5
