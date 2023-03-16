002bda8a257f66966abe6a9b18bf894b30154a99
Jordan Jared Tejera Yánez <Darkjordanrayo@gmail.com>
2023-03-16 16:43:08 +0000

    Configuration had changed

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,9 +1,8 @@
 module.exports = {
-  // HOST: "containers-us-west-168.railway.app",
-  HOST: "localhost",
-  USER: "root",
+  HOST: process.env.HOST,
+  USER: process.env.USER,
   PASSWORD: process.env.PASSWORD,
-  DB: "healthy_thinking",
+  DB: process.env.DB,
   dialect: "mysql",
   pool: {
     max: 5,
